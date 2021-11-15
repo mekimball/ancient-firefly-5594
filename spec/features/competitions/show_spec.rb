6858b9e80250d3f7421658cec95a428d3234b243
Matthew Kimball <mekimba6@gmail.com>
2021-11-15 09:54:42 -0700

    adds show page for competitions and test for competition info and team info

--- spec/features/competitions/show_spec.rb
+++ spec/features/competitions/show_spec.rb
@@ -4,8 +4,9 @@ RSpec.describe 'Competition Show Page', type: :feature do
   before do
     @competition_1 = Competition.create!(name: "Stanley Cup Finals", location: "Dallas", sport: "Hockey")
 
-    @team_1 = Team.create!(nickname: "Lightning", hometown: "Tampa")
+    @team_1 = Team.create!(nickname: "Lightning", hometown: "Tampa Bay")
     @team_2 = Team.create!(nickname: "Avalanche", hometown: "Denver")
+    @team_3 = Team.create!(nickname: "Panthers", hometown: "Florida")
 
     @player_1 = @team_1.players.create!(name: "Jim", age: 37)
     @player_2 = @team_1.players.create!(name: "Tim", age: 27)
@@ -15,8 +16,8 @@ RSpec.describe 'Competition Show Page', type: :feature do
     @player_5 = @team_2.players.create!(name: "Rob", age: 24)
     @player_6 = @team_2.players.create!(name: "Lob", age: 20)
 
-    CompetitionTeams.create!(competition: @competition_1, team: @team_1)
-    CompetitionTeams.create!(competition: @competition_1, team: @team_2)
+    CompetitionTeam.create!(competition: @competition_1, team: @team_1)
+    CompetitionTeam.create!(competition: @competition_1, team: @team_2)
 
     visit competition_path(@competition_1)
   end
@@ -26,6 +27,15 @@ RSpec.describe 'Competition Show Page', type: :feature do
       expect(page).to have_content(@competition_1.name)
       expect(page).to have_content(@competition_1.location)
       expect(page).to have_content(@competition_1.sport)
+    end
+
+    it 'shows name and hometown of all teams' do
+      expect(page).to have_content(@team_1.nickname)
+      expect(page).to have_content(@team_1.hometown)
+      expect(page).to have_content(@team_2.nickname)
+      expect(page).to have_content(@team_2.hometown)
+      expect(page).to_not have_content(@team_3.nickname)
+      expect(page).to_not have_content(@team_3.hometown)
     end
   end
 end
